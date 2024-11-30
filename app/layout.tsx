5b4d503ef969a8eb35a4d775edb3edf136a2cb43
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2024-11-30 23:38:01 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Dragon's Realm",
+  title: "Infernia",
   description: "Enter the Dragon's Realm and unleash your inner warrior",
 };
 
